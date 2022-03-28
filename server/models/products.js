a5dd99626dc579f678a37d277a9ea407a81e83f5
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-03-28 10:41:15 +0200

    Add products

---
+++ server/models/products.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+const path = require("path")
+const util = require("util") // Tar emot en callback och gör om den till en promise
+
+const readFile = util.promisify(fs.readFile)
+
+const readProducts = async () => {
+    const productsFile = path.join(__dirname, "prodcuts.json")
+    const productsJson = await readFile(productsFile)
+    return JSON.parse(productsJson)
+}
+
+module.exports = { readProducts }
